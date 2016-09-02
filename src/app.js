2bdb28b4f81cab21774a66655206ce21115f01de
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2016-09-03 00:42:08 +0300

    successfully loading data from json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 (function(){
 	'use strict';
 	window.onload = function() {
-		console.log('init');
+		var hangmanApp = angular.module('hangmanApp', []);
+		hangmanApp.factory('conundrums', function($http){
+			return {
+				getData: function(callback){
+					$http.get('data/data.json').success(callback);
+				}
+			};
+		});
+		hangmanApp.controller('HangmanParentController', function ($scope, conundrums){
+			conundrums.getData(function(data) {
+				$scope.conundrums = data;
+			});
+		});
+
+		angular.bootstrap(document, ['hangmanApp']);
 	};
 })();
